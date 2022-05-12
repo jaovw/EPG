2b72c4f47d65e5d3ea9d36b7c6822860801c23d9
joao.victor <joao.victor@superfestval.com.br>
2022-05-12 17:46:19 -0300

    mudando a forma da requisicao

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,9 +1,17 @@
-const paginaInicial = (req, res) => {
-    res.render('index')
+const axios = require('axios')
+
+async function getDados(diaAtual) {
+
+    const resultado = await axios(`https://epg-api.video.globo.com/programmes/1337?date=${diaAtual}`)
+
+    return resultado.data
 }
 
-const post = (req, res) => {
-    res.send('Sua nova rota de POST')
+async function getHora(diaAtual) {
+
+    const resultado = await axios(`https://epg-api.video.globo.com/programmes/1337?date=${diaAtual}`)
+    return resultado.data.programme.entries
+
 }
 
-module.exports = {paginaInicial, post}
+module.exports = {getDados, getHora}
