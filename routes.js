b383b577ab5ac053fc8cc869c3ab78d264c83c75
joao.victor <joao.victor@superfestval.com.br>
2022-05-12 17:47:21 -0300

    tratando dados da rota

--- routes.js
+++ routes.js
@@ -1,11 +1,40 @@
 const express = require('express')
 const route = express.Router()
-const {paginaInicial, post} = require('./src/controllers/homeController')
-const {contato} = require('./src/controllers/contatoContoller')
+const {getDados, getHora} = require('./src/controllers/homeController')
+const moment = require('moment')
+
+// const horaMoment = moment().add(3,'hours').format('HH:MM:DD')
+// const diaAtual = moment().format('YYYY-MM-DD')
 
 //  ROTAS DA HOME.
-route.get('/', (req, res) =>{
-    res.render('index')
+route.get('/',async (req, res) =>{
+
+
+    const data = new Date().toLocaleString('en-US', { hour12: false, timeZone: 'UTC' })
+    const diaAtual = new Date().toISOString().split('T')[0]
+    const[a, horaAtual] = data.split(' ')
+    
+
+    const d = await getDados(diaAtual)    
+    const t = await getHora(diaAtual, horaAtual)
+
+    let programacaoAtual = []
+    for(dado of t){ 
+
+        if(dado.human_end_time > horaAtual && dado.human_start_time <= horaAtual){
+
+            dado.title === null ? programacaoAtual.push(dado.description): programacaoAtual.push(dado.title)
+
+        }else {
+            'Algo deu errado'
+        }
+    }
+    // console.log(diaAtual, horaAtual)
+    // console.log('moment: ',horaMoment)
+    // console.log(diaAtual)
+
+    
+    res.render('index',{ api:d, horas: t ,programacaoAtual, title: 'EPG'})
 })
 
 module.exports = route
