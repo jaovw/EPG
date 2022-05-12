3d912fbf4e45e8b0f29ef785df7a92d378e218f4
joao.victor <joao.victor@superfestval.com.br>
2022-05-12 17:38:05 -0300

    Arrumando porta e arquivos estaticos

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const routes = require('./routes')
 const path = require('path')
 const expressLayouts = require('express-ejs-layouts')
 
-const port = 5000
+const port = process.env.PORT || 5000
 
 app.use(express.urlencoded({ extended: true}))
 
@@ -16,4 +16,11 @@ app.set('layout', './layouts/default')
 
 app.use(routes)
 
-app.listen(port, () => console.info(`Rodando na porta ${port}`))
+// STATICOS
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+
+
+
+
+app.listen(port, () => console.info(`Rodando em http://localhost:${port}`))
