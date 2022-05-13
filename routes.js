b929417b25d08dd466a1457c611fe1fa5c960896
joao.victor <joao.victor@superfestval.com.br>
2022-05-13 17:40:19 -0300

    Organizando e tratando novamente a Date

--- routes.js
+++ routes.js
@@ -1,43 +1,40 @@
 const express = require('express')
 const route = express.Router()
 const {getDataApi, getDadosApi} = require('./src/controllers/homeController')
-const moment = require('moment')
-
-// let horaMoment = moment().add(3,'hours').format('HH:MM:DD')
-// let diaMoment = moment().format('YYYY-MM-DD')
 
 //  ROTAS DA HOME.
 route.get('/',async (req, res) =>{
 
-    let data = new Date().getTime()
-    let dataAbreviada = Math.floor(data / 1000)
+    let dataAbreviada = Math.floor(new Date().getTime() / 1000)
 
     const dataParametro = new Date().toLocaleDateString()
+
     const [dia,mes,ano] = dataParametro.split('/')
 
     const diaParametro = `${ano}-${mes}-${dia}`
 
+    const dataFront = new Date().toLocaleString('pt-br', { hour12: false, timeZone: 'UTC',dateStyle: 'short'})
+
     const resultadoGetData = await getDataApi(diaParametro)    
-    const resultadoGetDados = await getDadosApi(diaParametro, dataAbreviada)
+    const resultadoGetDados = await getDadosApi(diaParametro)
 
     let programacaoAtual = []
     for(dado of resultadoGetDados){ 
 
         if(dado.end_time > dataAbreviada && dado.start_time <= dataAbreviada){
 
-            dado.title === null ? programacaoAtual.push(dado.description): programacaoAtual.push(dado.title)
+            // dado.title === null ? programacaoAtual.push(dado.program.name): programacaoAtual.push(dado.title)
+            programacaoAtual.push(dado.program.name)
 
         }else {
             'Algo deu errado'
         }
     }
-    // console.log('DIA >>',diaMoment,' HORA >>', horaMoment)
-    // console.log('novo: ',data, dataAbreviada)
-    // console.log(dataParametro,dataAbreviada)
-    // console.log(diaParametro)
 
-    
-    res.render('index',{ api:resultadoGetDados, data:resultadoGetData, programacaoAtual, title: 'EPG'})
+    // console.log(dataHorarioProg)
+    // console.log(dataAbreviada)
+
+    res.render('index',{ api:resultadoGetDados, data:resultadoGetData, programacaoAtual, title: 'EPG', dataHeader: dataFront})
 })
 
 module.exports = route
